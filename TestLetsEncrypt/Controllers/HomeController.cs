095d980de8290ef7e516f2bbcfd1aa04071eedf3
Carlos <carcarbe@gmail.com>
2020-01-02 18:24:43 +0100

    add home controller.

---
+++ TestLetsEncrypt/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLetsEncrypt.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+
+            return Ok("Hello, world.");
+        }
+    }
+}
